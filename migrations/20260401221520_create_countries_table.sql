f5ed221faa604fe184b1916813d02c4c15372754
O'Brien <obrienadedapo15@gmail.com>
2026-04-02 17:22:59 +0100

    fix(migrations): replace MariaDB-only collation with utf8mb4_unicode_ci

--- migrations/20260401221520_create_countries_table.sql
+++ migrations/20260401221520_create_countries_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE `countries` (
   `exchange_rate` double NULL,
   `estimated_gdp` double NULL,
   `flag_url` varchar(500) NOT NULL DEFAULT '',
-  `last_refreshed_at` datetime(3) NOT NULL,
+  `last_refreshed_at` datetime (3) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `idx_countries_name` (`name`)
-) COLLATE utf8mb4_uca1400_ai_ci;
+) COLLATE utf8mb4_unicode_ci;
